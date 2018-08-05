3a8e4fd41ea9c0da9b774295e1c76adf0d19c0f8
Michal <michalhz159@gmail.com>
2018-08-05 12:39:16 +0200

    Completely removed ConnectionData event since it can not be caught

--- ConnectionData.cs
+++ ConnectionData.cs
@@ -6,12 +6,9 @@ namespace Igor.TCP {
 		public string ipAddress { get; private set; }
 		public ushort port { get; private set; }
 
-		public event EventHandler<ConnectionData> OnConnectionDataParsed;
-
 		public ConnectionData(string ipAddress, ushort port) {
 			this.ipAddress = ipAddress;
 			this.port = port;
-			OnConnectionDataParsed(this, this);
 		}
 	}
 }
